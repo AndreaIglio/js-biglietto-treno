9eba18f2ce29e238fb9b22b2964b34f965555494
Andrea Iglio <andrea.iglio@gmail.com>
2020-11-13 15:58:50 +0100

    Completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,35 @@
 
 // Quanti km che si vogliono percorrere
 
-var kmTrip = Number(prompt("Quanti km vuoi percorrere?"));
+var kmPassenger = Number(prompt("Quanti km vuoi percorrere?"));
+
+// Quanti anni hai?
+
+var etaPassenger = Number(prompt("Quanti anni hai?"));
+
+// final price calc
+
+var ticketPrice = kmPassenger * 0.21;
+
+// final price underage
+
+var ticketPriceUnderage = ticketPrice - (ticketPrice * (20/100));
+
+// final price over65
+
+var ticketPriceOver65 = ticketPrice - (ticketPrice * (40/100));
+
+document.getElementById('ticketprice').innerHTML = ticketPrice;
+
+// Discount minorenni/over65
+
+if (etaPassenger < 18 && etaPassenger > 0)  {
+
+document.getElementById('ticketprice').innerHTML = ticketPriceUnderage
+
+}
+
+else if (etaPassenger >= 65) {
+    document.getElementById('ticketprice').innerHTML = ticketPriceOver65
+}
+
